9b0151e39ff786662958046e26557e8180b4e108
q3aql <q3aql@protonmail.ch>
2021-04-26 20:22:41 +0200

    Light Theme/Filters/Indexer/Fonts

---
+++ light/filters.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+rem # Apply filters
+call gen\jrepl.bat "Package or application ansi-gli-" "ANSI Greater and Less in ISO v" /f index.html /o -
+call gen\jrepl.bat "Package or application aria2-" "CLI tool for multiprotocol downloads v" /f index.html /o -
+call gen\jrepl.bat "Package or application aria2bt-tools-" "Python scripts to manage aria2 BitTorrent downloads v" /f index.html /o -
+call gen\jrepl.bat "Package or application clamTools" "Python Command-Line tools for ClamAV v" /f index.html /o -
+call gen\jrepl.bat "Package or application easy-samba-" "A basic GTK (zenity) or CLI interface for Samba v" /f index.html /o -
+call gen\jrepl.bat "Package or application drivers-linux-firmware_" "Updated drivers from Linux-Firmware for Debian/Ubuntu v" /f index.html /o -
+call gen\jrepl.bat "Package or application ip2e-" "Daemon to send your public IP (dynamic) by email when it is renewed v" /f index.html /o -
+call gen\jrepl.bat "Package or application irssi-" "IRC Client for CLI interface v" /f index.html /o -
+call gen\jrepl.bat "Package or application ks-tools-" "Tools for upload, convert and crop/mix video/audio files v" /f index.html /o -
+call gen\jrepl.bat "Package or application mktorrent-" "A simple command line utility to create BitTorrent metainfo files v" /f index.html /o -
+call gen\jrepl.bat "Package or application zenigrub-custom-" "A simple GTK (Zenity) interface to edit basic settings of GRUB v" /f index.html /o -
+call gen\jrepl.bat "Package or application archive-inr-" "Source code of archive-indexer bootstrap v" /f index.html /o -
---
+++ light/index-light.cmd
@@ -0,0 +1,85 @@
+@echo off
+
+rem ###########################################
+rem # Archive - Backup file indexer           #
+rem # Date: 30-03-2021                        #
+rem # Author: q3aql                           #
+rem # Contact: q3aql@protonmail.ch            #
+rem ###########################################
+
+rem # Filter variables
+set skip_files_list="-I css -I ico -I index.html -I index.php -I src"
+set dir_files=..\files
+set dir_files_scripts=..\scripts
+set dir_files_documents=..\documents
+
+rem # Filter variables
+set dirTemp=gen
+set listTemp=total-files.list
+set IndexerTempFolder=gen\indexer
+
+rem # Create initial HTML
+echo.
+echo * Creating initial HTML
+type gen\main-win.html > index.html
+type gen\main-scripts-win.html > scripts\index.html
+type gen\main-documents-win.html > documents\index.html
+
+rem # Build file list
+dir /B %dir_files% > gen\list-archive-files.txt
+dir /B %dir_files_scripts% > gen\list-archive-scripts.txt
+dir /B %dir_files_documents% > gen\list-archive-documents.txt
+
+rem # Count files
+type gen\list-archive-documents.txt | find /v "" /c > gen\count-archive-documents.txt
+type gen\list-archive-scripts.txt | find /v "" /c > gen\count-archive-scripts.txt
+type gen\list-archive-files.txt | find /v "" /c > gen\count-archive-files.txt
+set /p number_files=<gen\count-archive-files.txt
+set /p number_documents=<gen\count-archive-documents.txt
+set /p number_scripts=<gen\count-archive-scripts.txt
+
+rem # Set latest update
+echo ^<div class="mb-2 alert alert-success"^> >> index.html
+echo The last update was on %date% (%number_files% packages). >> index.html
+echo ^</div^> >> index.html
+echo ^<ul class="list-group"^> >> index.html
+
+rem # Set latest update (documents)
+echo ^<div class="mb-2 alert alert-success"^> >> documents\index.html
+echo The last update was on %date% (%number_documents% documents). >> documents\index.html
+echo ^</div^> >> documents\index.html
+echo ^<ul class="list-group"^> >> documents\index.html
+
+rem # Set latest update (scripts)
+echo ^<div class="mb-2 alert alert-success"^> >> scripts\index.html
+echo The last update was on %date% (%number_scripts% scripts). >> scripts\index.html
+echo ^</div^> >> scripts\index.html
+echo ^<ul class="list-group"^> >> scripts\index.html
+
+rem # Index files from files folder
+for /F %%f in (gen\list-archive-files.txt) do (
+echo + Indexing the file %%f
+echo ^<li class="list-group-item"^>^<a href="%dir_files%\%%f"^>%%f^</a^> - Package or application %%f ^(autoindexed^)^.^</li^> >>index.html
+)
+
+rem # Index files from scripts folder
+for /F %%f in (gen\list-archive-scripts.txt) do (
+echo + Indexing the file %%f
+echo ^<li class="list-group-item"^>^<a href="..\%dir_files_scripts%\%%f"^>%%f^</a^>^</li^> >> scripts\index.html
+)
+
+rem # Index files from documents folder
+for /F %%f in (gen\list-archive-documents.txt) do (
+echo + Indexing the file %%f
+echo ^<li class="list-group-item"^>^<a href="..\%dir_files_documents%\%%f"^>%%f^</a^>^</li^> >> documents\index.html
+)
+
+rem # Apply filters
+call filters.cmd
+
+rem # Create the final part 
+echo * Create final HTML
+type gen\footer.html >> index.html
+type gen\footer.html >> scripts\index.html
+type gen\footer.html >> documents\index.html
+
